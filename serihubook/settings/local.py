7c2976ad3446b44419b395600b3eb3c279ec4837
dokugakun <dokugakun.k789@outlook.jp>
2020-09-22 12:56:10 +0900

    logging config

--- serihubook/settings/local.py
+++ serihubook/settings/local.py
@@ -16,3 +16,50 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+# logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[{asctime}]:{levelname}:{module}:Process{process:d}:Thread{thread:d}:{message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '[{asctime}]:{levelname}:{module}:{message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'root': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django': {
+            'handlers': ['null'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['null'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'serihu': {
+            'handlers': ['null'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    }
+}
--- serihubook/settings/production.py
+++ serihubook/settings/production.py
@@ -12,3 +12,54 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {'default': dj_database_url.config()}
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '[{asctime}]:{levelname}:{module}:{message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        'root': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django': {
+            'handlers': ['null'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'serihu': {
+            'handlers': ['null'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
